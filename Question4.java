eea74a2b4369134a4a5413ad2f62203b524df2e4
Aaronlsk <148404832+Aaronlsk@users.noreply.github.com>
2024-04-15 15:30:39 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -5,13 +5,12 @@ public class Question4
   public static void main(String[] args)
   {
     Scanner in = new Scanner(System.in);
-    System.out.println("Enter an integer: ");
+    //System.out.println("Enter an integer: ");
     int number = in.nextInt();
     for (int i = number; i >= 1; i--) {
         for (int j = 1; j <= i; j++) {
             System.out.print("*");
         }
-        System.out.println();
     }
   }
 }
